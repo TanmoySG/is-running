e197c1006bf4bb9543eab3788e5ee0f02a137c1f
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2021-09-03 20:28:30 +0000

    added server

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, request
+import json
+from typing import Collection
+import shortuuid
+import secrets
+import random
+import hashlib
+from flask import Flask, request, jsonify, send_from_directory
+import os.path
+from os import path, write
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+
+CORS(app)
+
+def write_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+def alert(endpoint, response):
+    
 
 
 @app.route("/")
